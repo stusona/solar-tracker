66fb0069a63244a7769b387d411caca358be123c
Stu <supadupastu@gmail.com>
2019-09-22 21:57:15 -0700

    Motor class

---
+++ Motor.cpp
@@ -0,0 +1,29 @@
+/*
+ * Motor.cpp
+ */
+#include "Motor.h"
+#include "solar_tracker.h"
+
+/*
+ * motorForward
+ */
+void Motor::motorForward(int speed)
+{
+	// go forward
+}
+
+/*
+ * motorBackward
+ */
+void Motor::motorBackward(int speed)
+{
+	// go backward
+}
+
+/*
+ * motorStop
+ */
+void Motor::motorStop()
+{
+	// stahp
+}
---
+++ Motor.h
@@ -0,0 +1,16 @@
+/*
+ * Motor.h
+ */
+#ifndef MOTOR_H
+#define MOTOR_H
+
+class MOTOR
+{
+private:
+	int blah;
+public:
+	void motorForward(int speed);
+	void motorBackward(int speed);
+	void motorStop();
+}
+#endif
